6ed61995262ea9c26d291eade34dcb7e28117ed2
rovh <56371012+rovh@users.noreply.github.com>
2020-07-29 07:28:32 +0300

    Code

--- __init__.py
+++ __init__.py
@@ -35,57 +35,55 @@ bl_info = {
 
 # print(44444444444444)
 
-class SetLength(bpy.types.Operator):
+class Note_Actions(bpy.types.Operator):
     """Tooltip"""
-    bl_idname = "mesh.change_length"
-    bl_label = "Set Length " + name
+    bl_idname = "window_manager.export_note_text"
+    bl_label = "note text actions"
     bl_description = 'You can also assign shortcut \n How to do it: > right-click on this button > Assign Shortcut'
     # bl_options = {'REGISTER', 'UNDO'}
     bl_options = {'UNDO'}
 
-    plus_length: bpy.props.IntProperty(options = {"SKIP_SAVE"}) 
-    eyedropper: bpy.props.BoolProperty(options = {"SKIP_SAVE"})
-    draw:       bpy.props.BoolProperty(options = {"SKIP_SAVE"})
-    lengthbool_SKIP_SAVE: bpy.props.BoolProperty(options = {"SKIP_SAVE"})
-    @classmethod
-    def poll(cls, context):
-        return context.active_object is not None\
-            and context.active_object.mode in {'EDIT'}\
-            and context.active_object.type == "MESH"
+    action: StringProperty(options = {"SKIP_SAVE"})
+    # eyedropper: bpy.props.BoolProperty(options = {"SKIP_SAVE"})
+    # draw:       bpy.props.BoolProperty(options = {"SKIP_SAVE"})
+    # lengthbool_SKIP_SAVE: bpy.props.BoolProperty(options = {"SKIP_SAVE"})
+    # @classmethod
+    # def poll(cls, context):
+    #     return context.active_object is not None\
+    #         and context.active_object.mode in {'EDIT'}\
+            # and context.active_object.type == "MESH"
 
-    @classmethod
-    def description(cls, context, properties):
-        if properties.plus_length == 1:
-            return "Plus Length / Distance"
-        elif properties.plus_length == -1:
-            return "Minus Length / Distance"
-        elif properties.eyedropper == True:
-            return "Get Length / Distance"
-        else:
-            pass
+    # @classmethod
+    # def description(cls, context, properties):
+    #     if properties.plus_length == 1:
+    #         return "Plus Length / Distance"
+    #     elif properties.plus_length == -1:
+    #         return "Minus Length / Distance"
+    #     elif properties.eyedropper == True:
+    #         return "Get Length / Distance"
+    #     else:
+    #         pass
 
     def execute(self, context):
+        action = self.action
+        note_text_object = bpy.context.active_object.note_text_object
+        note_text_scene = bpy.context.scene.note_text_scene
+
+        main_text = bpy.data.texts['Text'].as_string()
+
+        # action = 'object'
         
-        if self.draw == False:
-            check(self) 
 
-        # Set values
-        length = bpy.context.window_manager.setprecisemesh.length
-        data_block_2 = bpy.context.window_manager.setprecisemesh.data_block_2
+        if action == 'object':
+            bpy.context.active_object.note_text_object = main_text
+        elif action == "object_get":
+            bpy.data.texts['Text'].clear()
+            bpy.data.texts['Text'].write(note_text_object)
+        elif action == "object_delete":
+            bpy.context.active_object.note_text_object = ""
+
+        return {'FINISHED'}
         
-        """Replace syntax"""
-        data_block_2 = data_block_2.replace(',', '.')
-        data_block_2 = data_block_2.replace('^', '**')
-        data_block_2 = data_block_2.replace(':', '/')
-        data_block_2 = data_block_2.replace('unit', str(length))
-        data_block_2 = data_block_2.replace('u', str(length))
-        # print(   re.search(r"\((\w+)\)", data_block_2)   )
-
-        # data_block_2 = data_block_2.replace( 'psqrt(r(A-Za-z0-9)' , 'sqrt(r(A-Za-z0-9_])')
-
-
-        script_input_2 = bpy.context.scene.script_input_2
-        length_unit = bpy.context.scene.unit_settings.length_unit
 
 class TEXT_PT_noter(Panel):
     bl_space_type = 'TEXT_EDITOR'
@@ -106,7 +104,10 @@ class TEXT_PT_noter(Panel):
         column.separator(factor = 1)
 
         box = column.box()
-        box.label(text = '11111111')
+        col = box.column(align = 1)
+        col.operator("window_manager.export_note_text", text = 'Object', icon = 'OBJECT_DATAMODE').action = "object"
+        col.operator("window_manager.export_note_text", text = '', icon = 'FILE_TICK').action = "object_get"
+        col.operator("window_manager.export_note_text", text = '', icon = 'TRASH').action = "object_delete"
 
 class Noter_Props (bpy.types.PropertyGroup):
     """
@@ -119,6 +120,7 @@ class Noter_Props (bpy.types.PropertyGroup):
 blender_classes = [
     TEXT_PT_noter,
     Noter_Props,
+    Note_Actions,
 ]
 
 blender_classes = Notes_list_blender_classes + blender_classes
@@ -137,6 +139,9 @@ def register():
     for blender_class in blender_classes:
         bpy.utils.register_class(blender_class)
 
+    bpy.types.Object.note_text_object = StringProperty()
+    bpy.types.Scene.note_text_scene = StringProperty()
+
     bpy.types.WindowManager.noter = PointerProperty(type=Noter_Props)
 
     bpy.types.Object.notes_list_object = CollectionProperty(type=Notes_List_Collection)
@@ -145,11 +150,13 @@ def register():
     bpy.types.Scene.notes_list_object = CollectionProperty(type=Notes_List_Collection)
     bpy.types.Scene.notes_list_scene_index = IntProperty()
     
-
 def unregister():
 
     for blender_class in blender_classes:
             bpy.utils.unregister_class(blender_class)
+
+    del bpy.types.Object.note_text_object
+    del bpy.types.Scene.note_text_scene
 
     del bpy.types.Object.notes_list_object
     del bpy.types.Object.notes_list_object_index
