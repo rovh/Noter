8201d7a925bbfaffd1e892a10ba43ccbaf718d69
rovh <56371012+rovh@users.noreply.github.com>
2020-09-02 05:40:20 +0300

    Code

--- Nodes.py
+++ Nodes.py
@@ -242,8 +242,6 @@ class Note_Node_Bool_Operator(bpy.types.Operator):
             bpy.data.node_groups[node_tree.name].nodes[self.name].mute = True
     
 
-        
-
         return {'FINISHED'}
 
 class Add_Nodes_Tree(bpy.types.Operator):
@@ -497,11 +495,12 @@ class MyCustomNode(Node, MyCustomTreeNode):
     # http://wiki.blender.org/index.php/Doc:2.6/Manual/Extensions/Python/Properties
     text: bpy.props.StringProperty()
     my_bool: bpy.props.BoolProperty()
-    draw_extra: bpy.props.StringProperty(default = "++")
+    draw_extra: bpy.props.StringProperty(default = "+++")
     
     image_bool: bpy.props.BoolProperty()
 
     image: bpy.props.PointerProperty(type= bpy.types.Image)
+
 
     # === Optional Functions ===
     # Initialization function, called when a new node is created.
@@ -548,6 +547,9 @@ class MyCustomNode(Node, MyCustomTreeNode):
     # def draw_buttons_ext(self, context, layout):
     def draw_buttons(self, context, layout):
 
+        text = self.text
+        draw_extra_count = self.draw_extra.count("+")
+
         if self.image_bool == True:
 
             row = layout.row()
@@ -556,63 +558,77 @@ class MyCustomNode(Node, MyCustomTreeNode):
             row.scale_y = 1.4
 
             try:
-                layout.separator()
 
                 self.image.name
 
+                layout.separator()
                 row = layout.row()
-                row.label(icon = "IMAGE_DATA")
+                row.label( icon = "IMAGE_DATA" )
                 row.operator("scene.noter_image",  icon = "EXPORT", text = 'View Image').my_image_name = self.image.name
                 row.scale_y = 1.7
+
+                layout.separator()
 
             except AttributeError:
                 pass
 
-        text = self.text
-        if text.count("\n") == 0:
-            layout.separator(factor = 1)
-            box = layout.box()
-            box.prop(self, "text", text = '')
-        else:
-            text_parts_list = text.split('\n')
-            layout.separator(factor = .5)
-            box = layout.box()
-            box = box.box()
-            col = box.column(align = 1)
-            for i in text_parts_list:
-                row = col.row(align = 1)
-                row.label(text = i)
-                row.scale_y = 0
-
-        draw_extra_count = self.draw_extra.count("+")
-
+        
         if draw_extra_count >= 1:
 
-            layout.separator(factor = 2)
 
-            row_header = layout.row()
 
-            ic = 'CHECKMARK' if self.mute else 'BLANK1'
 
-            row = row_header.row()
-            row.operator("node.noter_bool_operator",  icon = ic, text = '', depress = self.mute).name = self.name
-            row.alignment = 'LEFT'
-            if self.mute == True:
-                row.scale_y = 2.5
-                row.scale_x = 2.5
+            if text.count("\n") == 0:
+                # layout.separator(factor = 1)
+                box = layout.box()
+                box.prop(self, "text", text = '')
             else:
-                row.scale_y = 1
-                row.scale_x = 1
+                text_parts_list = text.split('\n')
+                layout.separator(factor = .5)
+                box = layout.box()
+                box = box.box()
+                col = box.column(align = 1)
+                for i in text_parts_list:
+                    row = col.row(align = 1)
+                    row.label(text = i)
+                    row.scale_y = 0
+
+            
+
 
             if draw_extra_count >= 2:
 
+
+
+
+                layout.separator(factor = 2)
+
+                row_header = layout.row()
+
+                ic = 'CHECKMARK' if self.mute else 'BLANK1'
+
                 row = row_header.row()
-                row.operator("node.noter_operator",  icon = 'IMPORT', text = '').action = f"node*{self.name}"
-                row.operator("node.noter_operator",  icon = 'EXPORT', text = '').action = f"node_get*{self.name}"
-                row.operator("node.noter_operator",  icon = 'TRASH', text = '').action = f"node_delete*{self.name}"
-                row.alignment = 'RIGHT'
-                row.scale_y = 1.6
-                row.scale_x = 1.6
+                row.operator("node.noter_bool_operator",  icon = ic, text = '', depress = self.mute).name = self.name
+                row.alignment = 'LEFT'
+                if self.mute == True:
+                    row.scale_y = 2.5
+                    row.scale_x = 2.5
+                else:
+                    row.scale_y = 1
+                    row.scale_x = 1
+
+
+
+
+                if draw_extra_count >= 3:
+
+                    row = row_header.row()
+                    row.operator("node.noter_operator",  icon = 'IMPORT', text = '').action = f"node*{self.name}"
+                    row.operator("node.noter_operator",  icon = 'EXPORT', text = '').action = f"node_get*{self.name}"
+                    row.operator("node.noter_operator",  icon = 'TRASH', text = '').action = f"node_delete*{self.name}"
+                    row.alignment = 'RIGHT'
+                    row.scale_y = 1.6
+                    row.scale_x = 1.6
 
     def update(self):
 
@@ -1168,6 +1184,8 @@ def insertNode(layout, type, text, settings = {}, icon = "NONE"):
         item.value = value
     return operator
 
+separator_factor_for_menus = .2
+
 class NODE_MT_add_menu_notes(bpy.types.Menu):
     bl_label = "Note"
 
@@ -1178,10 +1196,13 @@ class NODE_MT_add_menu_notes(bpy.types.Menu):
         props.use_transform = True
         props.type = "Noter_CustomNodeType"
 
-        layout.separator(factor = .1)
+        layout.separator(factor = separator_factor_for_menus)
 
-        insertNode(layout, "Noter_CustomNodeType", "Image Node", {  "draw_extra" : repr("++"),  "image_bool" : repr( True )   }, 'IMAGE_DATA')
+        insertNode(layout, "Noter_CustomNodeType", "Without extra buttons", {"draw_extra" : repr("++")}, 'OUTLINER_DATA_POINTCLOUD')
+        
+        layout.separator(factor = separator_factor_for_menus)
 
+        insertNode(layout, "Noter_CustomNodeType", "Without extra buttons +", {"draw_extra" : repr("+")}, 'LAYER_USED')
 
         # props = layout.operator("node.add_node", text = "Image Node", icon = 'IMAGE_DATA')
         # props.use_transform = True
@@ -1191,6 +1212,28 @@ class NODE_MT_add_menu_notes(bpy.types.Menu):
         # props.use_transform = True
         # props.type = "CompositorNodeImage"
 
+class NODE_MT_add_menu_image_notes(bpy.types.Menu):
+    bl_label = "Layout"
+
+    def draw(self, context):
+        layout = self.layout
+
+        # layout.operator_context = 'INVOKE_AREA'
+        
+        insertNode(layout, "Noter_CustomNodeType", "Image Node", {  "draw_extra" : repr("+++"),  "image_bool" : repr( True )   }, 'IMAGE_DATA')
+        
+        layout.separator(factor = separator_factor_for_menus)
+
+        insertNode(layout, "Noter_CustomNodeType", "Without extra buttons", {  "draw_extra" : repr("++"),  "image_bool" : repr( True )   }, 'OUTLINER_DATA_POINTCLOUD')
+
+        layout.separator(factor = separator_factor_for_menus)
+
+        insertNode(layout, "Noter_CustomNodeType", "Without extra buttons +", {  "draw_extra" : repr("+"),  "image_bool" : repr( True )   }, 'LAYER_USED')
+        
+        # layout.separator(factor = separator_factor_for_menus)
+
+        # insertNode(layout, "Noter_CustomNodeType", "Without extra buttons + +", {  "draw_extra" : repr(""),  "image_bool" : repr( True )   }, 'LAYER_USED')
+
 class NODE_MT_add_menu_othernotes(bpy.types.Menu):
     bl_label = "Other Notes"
 
@@ -1199,7 +1242,7 @@ class NODE_MT_add_menu_othernotes(bpy.types.Menu):
 
         insertNode(layout, "Noter_CustomNodeType", "Without extra buttons", {"draw_extra" : repr("+")}, 'OUTLINER_DATA_POINTCLOUD')
         
-        layout.separator(factor = .1)
+        layout.separator(factor = separator_factor_for_menus)
 
         insertNode(layout, "Noter_CustomNodeType", "Without extra buttons +", {"draw_extra" : repr("")}, 'LAYER_USED')
 
@@ -1215,7 +1258,7 @@ class NODE_MT_add_menu_layout(bpy.types.Menu):
         props.use_transform = True
         props.type = "NodeReroute"
 
-        layout.separator(factor = .1)
+        layout.separator(factor = separator_factor_for_menus)
         
         props = layout.operator("node.add_node", text = "Frame", icon = 'MATPLANE')
         props.use_transform = True
@@ -1241,11 +1284,13 @@ def add__NODE_MT_add(self, context):
             layout.separator(factor = 1)
 
             # layout.operator('node.add_search', text = "Note", icon = 'FILE')
-            layout.menu("NODE_MT_add_menu_notes", text = "Note", icon = "FILE")
+            layout.menu("NODE_MT_add_menu_notes", text = "Notes", icon = "FILE")
 
             layout.separator(factor = factor)
 
-            layout.menu("NODE_MT_add_menu_othernotes", text = "Other Notes", icon = 'DOCUMENTS')
+            
+            layout.menu("NODE_MT_add_menu_image_notes", text = "Image Notes", icon = 'IMAGE_DATA')
+            # layout.menu("NODE_MT_add_menu_othernotes", text = "Other Notes", icon = 'DOCUMENTS')
 
             layout.separator(factor = factor)
 
@@ -1291,7 +1336,7 @@ node_categories = [
     #     }),
     # ]),
 
-    MyNodeCategory('OTHERNODES', "", items=[
+    MyNodeCategory('OTHERNODES', "All Nodes", items=[
         
         NodeItem("Noter_CustomNodeType", label="Note Nodes"
         ),
@@ -1338,6 +1383,7 @@ Nodes_blender_classes = (
     NODE_MT_add_menu_layout,
     NODE_MT_add_menu_othernotes,
     NODE_MT_add_menu_notes,
+    NODE_MT_add_menu_image_notes,
     # Noter_NodeSearch,
     
 )
